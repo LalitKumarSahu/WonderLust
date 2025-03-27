17a2b94d1c784491daf3e0e5265748baa3842ee7
Lalit Kumar <shahulalit5@gmail.com>
2025-03-27 12:19:52 +0530

    Updated filter UI

--- util/wrapAsync.js
+++ util/wrapAsync.js
@@ -1,5 +1,5 @@
-module.exports = (fn) =>{
-  return (req, res,next) =>{
-    fn(req,res,next).catch(next);
+module.exports = (fn) =>{ // ye req,res,next ko accept krega
+  return (req, res,next) =>{ // ek new fcn jo req,res,next ko accept krega
+    fn(req,res,next).catch(next); // ek promise return karta hai agr error hai to catch kar dega
   }
 }
